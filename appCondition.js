8ac9558a02252311d7cdf31ea44036fc77e595c9
PhyoLattLattLwin <phyolatlatlwin@gicjp.com>
2023-08-08 16:04:39 +0630

    Conditional Operators

---
+++ appCondition.js
@@ -0,0 +1,13 @@
+let x = 100;
+let y = 10;
+let c = "10";
+
+let z = x != y;
+let a = x > y;
+let b = y===c;
+
+console.log(a);
+console.log(b);
+console.log(z);
+console.log(typeof x);
+console.log(typeof c);
