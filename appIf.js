ba0bca5169f103f08e56cfc3e3c2809a951da616
PhyoLattLattLwin <phyolatlatlwin@gicjp.com>
2023-08-08 16:30:42 +0630

    switchStatement

--- appIf.js
+++
@@ -1,7 +0,0 @@
-let age = 20;
-if(40 < 5) {
-    console.log("True => answer");
-}
-else {
-    console.log("False => answer");
-}
---
+++ appSwitch.js
@@ -0,0 +1,7 @@
+let color ="red";
+switch(color) {
+    case "green" : console.log("it is green"); break;
+    case "blue" : console.log("it is blue"); break;
+    default : console.log("I cannot guess");
+    
+}
