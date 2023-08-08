f30b547b206ef76ff10108e7f6aa538777921f21
PhyoLattLattLwin <phyolatlatlwin@gicjp.com>
2023-08-08 16:21:58 +0630

    ifStatement

--- appControlFlow.js
+++
@@ -1,7 +0,0 @@
-let age = 20;
-if(4 < 5) {
-    console.log("this is the answer");
-}
-else {
-    console.log("answer is wrong");
-}
---
+++ appIf.js
@@ -0,0 +1,7 @@
+let age = 20;
+if(40 < 5) {
+    console.log("True => answer");
+}
+else {
+    console.log("False => answer");
+}
