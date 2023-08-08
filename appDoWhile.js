fd5ab4c4cdf400beb399061da36d1b8649445207
PhyoLattLattLwin <phyolatlatlwin@gicjp.com>
2023-08-08 16:44:54 +0630

    dowhile

---
+++ appDoWhile.js
@@ -0,0 +1,5 @@
+let i = 0;
+do {
+    console.log("Hello world " + i);
+    i++;
+} while(i<1000);
--- appSwitch.js
+++
@@ -1,7 +0,0 @@
-let color ="red";
-switch(color) {
-    case "green" : console.log("it is green"); break;
-    case "blue" : console.log("it is blue"); break;
-    default : console.log("I cannot guess");
-    
-}
