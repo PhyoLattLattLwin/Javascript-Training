67eb1c0d89531da77a45cd9a8dd3e6f67df47496
PhyoLattLattLwin <phyolatlatlwin@gicjp.com>
2023-08-13 23:53:36 +0630

    arrowFunction

---
+++ arrowFunction.js
@@ -0,0 +1,18 @@
+let url = "https://jsonplaceholder.typicode.com/posts";
+
+let names = ["Bo Bo", "Mo Mo", "Po Po", "No No", "So So"];
+let students = [
+    {
+        name : "Mg Mg",
+        age : 20,
+        school : "ygn"
+    },
+
+    {
+        name : "Aung Aung",
+        age : 30,
+        school : "mdy"
+    }
+];
+
+students.map((stu) => console.log(stu.name))
